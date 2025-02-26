a1ab92383d11603970b633be1c5ce38f069755c5
ferdisetyoamanda <setyoamanda23@gmail.com>
2025-02-26 19:00:33 +0700

    Laporan Semua

--- src/main/java/com/juaracodingsqa20/kelompok3/drivers/utils/DriverWaitHelper.java
+++ src/main/java/com/juaracodingsqa20/kelompok3/drivers/utils/DriverWaitHelper.java
@@ -2,10 +2,23 @@ package com.juaracodingsqa20.kelompok3.drivers.utils;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.juaracodingsqa20.kelompok3.drivers.DriverSingleton;
 
 public class DriverWaitHelper {
     public final static WebDriverWait DRIVER_WAIT = new WebDriverWait(DriverSingleton.getDriver(), Duration.ofSeconds(30));
+
+    public static WebElement waitForVisibility(WebDriver driver, By searchInput, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'waitForVisibility'");
+    }
+
+    public static WebElement waitForClickable(WebDriver driver, By searchButton, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'waitForClickable'");
+    }
 }
